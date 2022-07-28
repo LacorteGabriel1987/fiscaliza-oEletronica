1d97865747486d13cf61f440af90600a2baab9fb
Gabriel Lacorte <gabrielslacorte@hotmail.com>
2022-07-28 12:58:10 -0300

    Ajustando argumento js

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ function calcular () {
     res.innerHTML = `<p> Sua velocidade atual é de <strong>${vel} km/h.</strong></p>`;
     if (vel > 60) {
         res.innerHTML +=`<p> Você está <strong>MULTADO</strong> por excesso de velocidade!</P>`;
+        res.style.background = 'red'
+
     } else if(vel <10 || vel >= 0) {
         res.innerHTML +=`<p> <strong>Desenvolve </strong> meu filho(a) </p>`;
+        res.style.background = 'green'
+
     }   if (vel <0){
 
         res.innerHTML += `<p> [ERRO] Número inválido </p>`;
+        res.style.background = 'yellow'
     }
 
     res.innerHTML +=`<p> Dirija sempre com cinto de segurança!</p>`;
 }
+    
