74170fefa7b8dc0cfdcb3afd94dc0aee91ff9496
Gabriel Lacorte <gabrielslacorte@hotmail.com>
2022-07-28 14:50:25 -0300

    Alterando a ordem do código js

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ function calcular () {
     var res = window.document.getElementById("res")
     var vel = Number(textv.value)
     res.innerHTML = `<p> Sua velocidade atual é de <strong>${vel} km/h.</strong></p>`;
-    if (vel > 60) {
-        res.innerHTML +=`<p> Você está <strong>MULTADO</strong> por excesso de velocidade!</P>`;
-        res.style.background = 'red'
 
-    } else if(vel <10 || vel >= 0) {
-        res.innerHTML +=`<p> <strong>Desenvolve </strong> meu filho(a) </p>`;
+    if (vel < 0 ) {
+        res.innerHTML +=`<p> [ERRO] Número inválido </p>`;
+        res.style.background = 'yellow'
+
+    } else if (vel >= 0 && vel <= 10) {
+        res.innerHTML +=`<p> <strong>Desenvolve</strong> meu filho(a) </p>`;
         res.style.background = 'green'
 
-    }   if (vel <0){
+    }   if (vel > 60){
 
-        res.innerHTML += `<p> [ERRO] Número inválido </p>`;
-        res.style.background = 'yellow'
+        res.innerHTML += `<p> [ERRO] Você está <strong> MULTADO</strong> por excesso de velocidade </p>`;
+        res.style.background = 'red'
     }
 
     res.innerHTML +=`<p> Dirija sempre com cinto de segurança!</p>`;
 }
-    
+
